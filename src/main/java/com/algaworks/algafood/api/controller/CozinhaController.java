e4d43bb9a8e925facb2cf0038b5e904b05ef8297
dsouzajm <danielsouza.jm@gmail.com>
2022-05-26 19:50:39 -0300

    Criacao de EstadoController

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +24,10 @@ public class CozinhaController {
 		
 		return cozinhaRepository.listar();
 	}	
+	
+	@GetMapping("/{id}")
+	public Cozinha buscar(@PathVariable Long id) {
+		
+		return cozinhaRepository.buscar(id);
+	}
 }
--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -6,10 +6,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@JsonRootName("cozinha")
 @Entity
 @Getter
 @Setter
@@ -20,6 +26,7 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@JsonProperty("titulo")
 	@EqualsAndHashCode.Exclude
 	@Column(nullable = false)
 	private String nome;
