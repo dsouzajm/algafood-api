c87a559be9c9962607d6fa129e9de00521c17427
dsouzajm <danielsouza.jm@gmail.com>
2023-07-15 14:41:06 -0300

    Usando Criteria API com predicates simples.

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepositoryImpl.java
@@ -8,6 +8,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -22,10 +24,15 @@ public class RestauranteRepositoryImpl implements CustomizedRestauranteRepositor
 
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Restaurante> criteriaQuery = criteriaBuilder.createQuery(Restaurante.class);
-        criteriaQuery.from(Restaurante.class);
+        Root<Restaurante> root = criteriaQuery.from(Restaurante.class);
+
+        Predicate likeNome = criteriaBuilder.like(root.get("nome"), "%" + nome + "%");
+        Predicate taxaFreteMaiorQue = criteriaBuilder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+        Predicate taxaFreteMenorQue = criteriaBuilder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+
+        criteriaQuery.where(likeNome, taxaFreteMaiorQue, taxaFreteMenorQue);
 
         TypedQuery<Restaurante> query = entityManager.createQuery(criteriaQuery);
-
         return query.getResultList();
     }
 }
