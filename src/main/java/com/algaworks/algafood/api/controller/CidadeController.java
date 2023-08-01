a07ee2a30835a48973d8d28cc851e33a49178854
dsouzajm <danielsouza.jm@gmail.com>
2023-08-01 10:14:59 -0300

    Formatando o codigo.

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -14,68 +14,45 @@ import java.util.List;
 @RestController
 @RequestMapping("/cidades")
 public class CidadeController {
-
     @Autowired
     CadastroCidadeService cadastroCidadeService;
-
     @GetMapping
-    public List<Cidade> listar(){
-
+    public List<Cidade> listar() {
         return cadastroCidadeService.listar();
     }
-
     @RequestMapping("/{id}")
-    public ResponseEntity<?> buscar(@PathVariable("id") Long id){
-
+    public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
         try {
-
             Cidade cidade = cadastroCidadeService.buscar(id);
             return ResponseEntity.ok(cidade);
-
-        } catch (EntidadeNaoEncontradaException e){
-
+        } catch (EntidadeNaoEncontradaException e) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(e.getMessage());
         }
     }
-
     @PostMapping
-    public ResponseEntity<?> adicionar(@RequestBody Cidade cidade){
-
+    public ResponseEntity<?> adicionar(@RequestBody Cidade cidade) {
         try {
-
             Cidade cidadePersistida = cadastroCidadeService.salvar(cidade);
             return ResponseEntity.ok(cidade);
-
-        } catch (EntidadeNaoEncontradaException e){
-
+        } catch (EntidadeNaoEncontradaException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
-
     @PutMapping("/{id}")
-    public ResponseEntity<?> atualizar(@PathVariable("id") Long id, @RequestBody Cidade cidade){
-
+    public ResponseEntity<?> atualizar(@PathVariable("id") Long id, @RequestBody Cidade cidade) {
         try {
-
             Cidade cidadePersistida = cadastroCidadeService.atualizar(id, cidade);
             return ResponseEntity.ok(cidadePersistida);
-
-        } catch (PropriedadeNaoEncontradaException | EntidadeNaoEncontradaException e){
-
+        } catch (PropriedadeNaoEncontradaException | EntidadeNaoEncontradaException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
-
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> excluir(@PathVariable("id") Long id){
-
+    public ResponseEntity<?> excluir(@PathVariable("id") Long id) {
         try {
-
             cadastroCidadeService.excluir(id);
             return ResponseEntity.ok().build();
-
-        } catch (EntidadeNaoEncontradaException e){
-
+        } catch (EntidadeNaoEncontradaException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -25,14 +25,11 @@ import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 @RestController
 @RequestMapping("/cozinhas")
 public class CozinhaController {
-	
 	@Autowired
 	private CadastroCozinhaService cadastroCozinhaService;
-	
 	@ResponseStatus(value = HttpStatus.OK)
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Cozinha> listar() {
-		
 		System.out.println("CHAMOU JSON");		
 		return cadastroCozinhaService.listar();
 	}		
@@ -46,61 +43,41 @@ public class CozinhaController {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
-		
-		Cozinha cozinha = cadastroCozinhaService.buscar(id);				
-		
+		Cozinha cozinha = cadastroCozinhaService.buscar(id);
 		if(cozinha != null){
 			return ResponseEntity.ok(cozinha);
 		}
-		
 		return ResponseEntity.noContent().build();		
 	}
-	
 	@GetMapping("/nao_existe")
 	public ResponseEntity<Cozinha> buscarCaminhoRemovido() {
-		
-		HttpHeaders headers = new HttpHeaders();		
+		HttpHeaders headers = new HttpHeaders();
 		headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
-		
 		return ResponseEntity.status(HttpStatus.TEMPORARY_REDIRECT).headers(headers).build();
 	}
-	
 	@PostMapping
 	@ResponseStatus(value = HttpStatus.CREATED)
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
-		
 		return cadastroCozinhaService.salvar(cozinha);		
 	}
-	
 	@PutMapping("/{id}")
 	public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody Cozinha cozinha){
-		
 		try {
-			
 			Cozinha cozinhaAtual = cadastroCozinhaService.atualizar(id, cozinha);
 			return ResponseEntity.ok(cozinhaAtual);
-			
 		} catch (EntidadeNaoEncontradaException e) {
-		
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
 		}
 	}
-	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Cozinha> excluir(@PathVariable Long id){
-		
 		try {
-		
 			cadastroCozinhaService.excluir(id);			
 			return ResponseEntity.ok().build();
-			
 		} catch (EntidadeNaoEncontradaException e) {
-			
 			System.out.println(e);
-			return ResponseEntity.notFound().build();	
-			
+			return ResponseEntity.notFound().build();
 		} catch (EntidadeEmUsoException e) {
-			
 			System.out.println(e);
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
